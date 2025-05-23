8ff888882543eb203989b7fb1b44d1e6b2b4d99c
fang <a643722351@gmail.com>
2025-05-23 14:26:55 +0800

    家里的

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["172.16.243.60"];
-pub const RS_PUB_KEY: &str = "ki8QJemKm6ok2Pn76X8EtMgowLGCISFSdgAxwIY7Cqw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["sunnyun.cn"];
+pub const RS_PUB_KEY: &str = "7jD0oN4Qp+EPhehnRtDIcTZadQYMfywzRmIuA3TR6Xw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
